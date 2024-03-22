7a99e09aa9d176b8aeb4014b3c55400e3f54f172
Marina Tanaka <huttenloucher@gmail.com>
2024-03-22 18:23:56 -0300

    Refatorando

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Dimensions, Button } from "react-native";
+import { View, StyleSheet, Dimensions, Button, Alert } from "react-native";
 import MapView, { Marker, Polyline } from "react-native-maps";
 import * as Location from "expo-location";
+
+// Funções utilitárias
+import { calculateDistance, formatTime } from "./utils/funcoes.js";
 
 export default function App() {
   const [initialLocation, setInitialLocation] = useState(null); // Localização Inicial
@@ -10,84 +13,96 @@ export default function App() {
   const [startTime, setStartTime] = useState(null); // Começar Contagem
   const [elapsedTime, setElapsedTime] = useState(0); // Pausa
   const [isRecording, setIsRecording] = useState(false); // Gravando
-  const [errorMsg, setErrorMsg] = useState(null); // Mensagem de Erro
   const [coordinates, setCoordinates] = useState([]); // Cordenadas
 
+  // useEffect de permissão e localização inial
   useEffect(() => {
-    (async () => {
+    async () => {
       // Variavel guardando a permisão
-      let { status } = await Location.requestForegroundPermissionsAsync();
+      const { status } = await Location.requestForegroundPermissionsAsync();
 
       // Condicional para localização negada
       if (status !== "granted") {
-        setErrorMsg("Permissão de localização negada");
+        Alert.alert(
+          "Permissão Necessária",
+          "Ative a localização para usar o app."
+        );
         return;
       }
 
-      // Variavel guardando localização inicial recebendo a localização atual
-      let initialLocation = await Location.getCurrentPositionAsync({});
+      // Obtendo a localização atual
+      const initialLocation = await Location.getCurrentPositionAsync({});
 
-      // State
+      // State para centralização inicial do usuario
       setInitialLocation(initialLocation);
       setCurrentLocation(initialLocation);
-    })(); // Função imediata para o Effect funcionar como um async/await
-  }, []);
+    },
+      [];
+  })(); // Função imediata usada para o Effect funcionar como um async/await ou Função assíncrona IIFE (Immediately Invoked Function Expression)
 
   // UseEffect com parametro para funcionamento da localização atual
   useEffect(() => {
-    // Condicional
-    if (currentLocation) {
-      const { latitude, longitude } = currentLocation.coords; // Objeto com mais informações da latitude e longitude
-
-      // Animação do mapa
+    // Função para animar o mapa para a nova localização
+    const animateMap = ({ latitude, longitude }) => {
       mapRef.current.animateToRegion({
         latitude,
         longitude,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.05,
       });
-    }
-  }, [currentLocation]); // Parametro (currentLocation)
+    };
 
-  const mapRef = React.createRef(); // Guardando a referencia no mapRef
+    // Condicional caso usuario tenha feito permissão
+    if (currentLocation) {
+      // Extrai latitude e longitude da propriedade coords de currentLocation
+      const { latitude, longitude } = currentLocation.coords;
+      // Chama a função animateMap com a latitude e longitude da localização atual
+      animateMap({ latitude, longitude });
+    }
+
+    // Dependencia do efeito: só reexecutar quando a currentLocation mudar
+  }, [currentLocation]);
+
+  // Função que cria um objeto de referência atraves do hook React.createRef
+  const mapRef = React.createRef(null);
 
   // Função para começar a gravar
   const startRecording = async () => {
-    // Variavel guardando a função de localização atual
-    let initialLocation = await Location.getCurrentPositionAsync({});
+    // Obter a localização atual
+    const initialLocation = await Location.getCurrentPositionAsync({});
 
-    // States
-    setInitialLocation(initialLocation);
-    setCurrentLocation(initialLocation);
-    setStartTime(new Date().getTime());
-    setIsRecording(true);
-    setCoordinates([initialLocation.coords]);
+    // Atualizar states
+    setInitialLocation(initialLocation); // Valor inicial
+    setCurrentLocation(initialLocation); // Armazena a localização atual e sempre sera atualizado quando a localização do usuário mudar
+    setStartTime(new Date().getTime()); // Armazena o momento em que a gravação é iniciada
+    setIsRecording(true); // Indica se a gravação está em andamento
+    setCoordinates([initialLocation.coords]); // Armazena um array de coordenadas do usuário ao longo do tempo.
 
-    // Variavel guardando função que monitora o movimento do usuario com o primeiro parametro de objeto vazio que recebe a option "default" e uma callback
-    let subscriber = await Location.watchPositionAsync({}, (newLocation) => {
-      // Atualização do state recebendo a nova localização
-      setCurrentLocation(newLocation);
+    // Monitorar movimento do usuário
+    useEffect(() => {
+      // Função para atualizações da localização em tempo real, recebe dois parametros o primeiro sendo um objeto vazio que pode ser usado para configurar opções da API de localização e o segundo parametro uma função callback chamada sempre que a nova posição do usuário for obtida
+      const subscriber = Location.watchPositionAsync({}, (newLocation) => {
+        // Atualizar state com nova localização
+        setCurrentLocation(newLocation);
 
-      // Variavel distancia guardando a função de calcular a distancia
-      let distance = calculateDistance(
-        initialLocation.coords.latitude,
-        initialLocation.coords.longitude,
-        newLocation.coords.latitude,
-        newLocation.coords.longitude
-      );
+        // Calcular a distancia percorrida (Calcula a distância entre a localização inicial e a nova)
+        const distance = calculateDistance(
+          initialLocation.coords.latitude,
+          initialLocation.coords.longitude,
+          newLocation.coords.latitude,
+          newLocation.coords.longitude
+        );
 
-      // States
-      setTotalDistance(totalDistance + distance);
-      setElapsedTime((new Date().getTime() - startTime) / 1000);
-      setCoordinates([...coordinates, newLocation.coords]);
-    });
+        // Atualizar state ( Atualiza a distância total, tempo decorrido e array de coordenadas)
+        setTotalDistance(totalDistance + distance);
+        setElapsedTime((new Date().getTime() - startTime) / 1000);
+        setCoordinates([...coordinates, newLocation.coords]);
+      });
 
-    // Die
-    return () => {
-      if (subscriber) {
-        subscriber.remove();
-      }
-    };
+      return () => subscriber.remove(); // Limpar listener ao desmontar o componente
+
+      // Dependencia do efeito: só reexecutar quando a initialLocation mudar
+    }, [initialLocation]);
   };
 
   // Função parar gravação
@@ -98,103 +113,71 @@ export default function App() {
     setElapsedTime(0);
   };
 
-  // Função para calcular distancia
-  const calculateDistance = (lat1, lon1, lat2, lon2) => {
-    // Raio da Terra
-    const R = 6371;
-
-    // Distancia da Latitude
-    const dLat = (lat2 - lat1) * (Math.PI / 180);
-
-    // Distancia da Longitude
-    const dLon = (lon2 - lon1) * (Math.PI / 180);
-
-    // Constante guardando a primeira parte da formula de Haversine
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(lat1 * (Math.PI / 180)) *
-        Math.cos(lat2 * (Math.PI / 180)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-
-    // Constante guardando a distancia final
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    // Distancia recebendo o raio vezes a distancia final
-    const distance = R * c;
-
-    // Retornando a const Distancia
-    return distance;
-  };
-
-  // Tratativa de erros
-  let text = "Aguardando permissões...";
-  if (errorMsg) {
-    text = errorMsg;
-  }
-
   return (
-    <View style={styles.container}>
-      {/* Componente do map */}
-      <MapView
-        ref={mapRef}
-        style={styles.map}
-        // Parametro de região inicial recebendo objeto com state de localização inicial
-        initialRegion={{
-          latitude: initialLocation
-            ? initialLocation.coords.latitude
-            : -23.55052,
-          longitude: initialLocation
-            ? initialLocation.coords.longitude
-            : -46.633308,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        {/* Localização Inicial com condicional para aparecer o Maker na primeira localização */}
-        {initialLocation && (
-          <Marker
-            coordinate={{
-              latitude: initialLocation.coords.latitude,
-              longitude: initialLocation.coords.longitude,
-            }}
-            title="Sua Localização Inicial"
-            description="Você começou aqui"
-          />
-        )}
+    <>
+      <View style={styles.container}>
+        {/* Componente do map */}
+        <MapView
+          ref={mapRef}
+          style={styles.map}
+          initialRegion={{
+            latitude: initialLocation
+              ? initialLocation.coords.latitude
+              : -23.55052,
+            longitude: initialLocation
+              ? initialLocation.coords.longitude
+              : -46.633308,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {/* Marker na Localização Inicial  */}
+          {initialLocation && (
+            <Marker
+              coordinate={{
+                latitude: initialLocation.coords.latitude,
+                longitude: initialLocation.coords.longitude,
+              }}
+              title="Sua Localização Inicial"
+              description="Você começou aqui"
+            />
+          )}
 
-        {/* Localização Atual para aparecer o Maker na localização atual */}
-        {currentLocation && (
-          <Marker
-            coordinate={{
-              latitude: currentLocation.coords.latitude,
-              longitude: currentLocation.coords.longitude,
-            }}
-            title="Sua Localização Atual"
-            // Mostrando valores de quantos km e tempo que o usuario andou
-            description={`Você andou ${totalDistance.toFixed(
-              2
-            )} km em ${formatTime(elapsedTime)}`}
-          />
-        )}
+          {/* Marker na Localização Atual  */}
+          {currentLocation && (
+            <Marker
+              coordinate={{
+                latitude: currentLocation.coords.latitude,
+                longitude: currentLocation.coords.longitude,
+              }}
+              title="Sua Localização Atual"
+              description={`Você andou ${totalDistance.toFixed(
+                2
+              )} km em ${formatTime(elapsedTime)}`}
+              pinColor="blue" // Cor azul para o marcador atual
+            />
+          )}
 
-        {/* Marcador de percuso */}
-        <Polyline
-          coordinates={coordinates}
-          strokeWidth={5}
-          strokeColor="rgba(255,0,0,0.5)"
-        />
-      </MapView>
+          {/* Marcador de percuso */}
+          {coordinates.length > 1 && (
+            <Polyline
+              coordinates={coordinates}
+              strokeColor="rgba(255,0,0,0.5)"
+              strokeWidth={5}
+            />
+          )}
+        </MapView>
 
-      {/* Botão de gravação */}
-      <View style={styles.buttonContainer}>
-        {isRecording ? (
-          <Button title="Parar Gravação" onPress={stopRecording} />
-        ) : (
-          <Button title="Iniciar Gravação" onPress={startRecording} />
-        )}
+        {/* Botão de gravação */}
+        <View style={styles.buttonContainer}>
+          {isRecording ? (
+            <Button title="Parar Gravação" onPress={stopRecording} />
+          ) : (
+            <Button title="Iniciar Gravação" onPress={startRecording} />
+          )}
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -215,12 +198,3 @@ const styles = StyleSheet.create({
     bottom: 20,
   },
 });
-
-const formatTime = (seconds) => {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const secs = Math.floor(seconds % 60);
-  return `${hours.toString().padStart(2, "0")}:${minutes
-    .toString()
-    .padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
-};
